b9a23e2f60cf26a9a7ef5c2d9a10eba8e5feee3a
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-12-29 23:09:50 -0700

    Reducer index built. Redux work almost done...

--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -6,3 +6,19 @@ import queueReducer from "../reducers/queueReducer.js";
 import searchReducer from "../reducers/searchReducer.js";
 import sessionReducer from "../reducers/sessionReducer.js";
 import usersReducer from "../reducers/usersReducer.js";
+
+export const reducers = () => {
+  combineReducers({
+    queue: queueReducer,
+    playback: playbackReducer,
+    session: sessionReducer,
+    users: usersReducer,
+    search: searchReducer,
+    devices: devicesReducer,
+  });
+};
+
+export const getDevices = (state) => fromDevices.getDevices(state.devices);
+
+export const getIsFetchingDevices = (state) =>
+  fromDevices.getIsFetching(state.devices);
